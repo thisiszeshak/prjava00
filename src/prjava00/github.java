831c5ea9b2677344c4ee85e9c3d232977535a64b
Adrià Rivero <adriariverosegarra@gmail.com>
2021-10-24 18:30:17 +0200

    Versió 0.2 del projecte prjava00

--- src/prjava00/github.java
+++ src/prjava00/github.java
@@ -1,11 +1,25 @@
 package prjava00;
+import java.net.*;
 
 public class github {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO code application logic here
 		System.out.println("Hola món");
-		System.out.println("Versió 0.1 del projecte prjava00");
+		System.out.println("versió 0.2 del projecte prjava00");
+		try {
+		InetAddress addr = InetAddress.getLocalHost();
+		String ipAddr = addr.getHostAddress();
+		String hostname = addr.getHostName();
+		System.out.println("hostname="+hostname);
+		System.out.println("Adreça IP: " + ipAddr);
+		System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+		System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+		System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+		System.out.println("Versió OS: " + System.getProperty("os.version"));
+		}
+		catch (UnknownHostException e) {
+		e.printStackTrace();
+		}
 	}
-
 }
